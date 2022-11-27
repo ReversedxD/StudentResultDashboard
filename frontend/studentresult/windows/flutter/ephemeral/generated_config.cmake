8200ab947d819dbe418be294b501e9d3670328b8
ReversedxD <dudejasiddharth@gmail.com>
2022-11-28 05:29:13 +0530

    push all project files

---
+++ frontend/studentresult/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Projects\\MajorProject\\frontend\\studentresult" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Flutter\\flutter"
+  "PROJECT_DIR=D:\\Projects\\MajorProject\\frontend\\studentresult"
+  "FLUTTER_ROOT=C:\\Flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Projects\\MajorProject\\frontend\\studentresult\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Projects\\MajorProject\\frontend\\studentresult"
+  "FLUTTER_TARGET=D:\\Projects\\MajorProject\\frontend\\studentresult\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Projects\\MajorProject\\frontend\\studentresult\\.dart_tool\\package_config.json"
+)
